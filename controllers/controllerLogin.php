3bc5296a93674949045b45c31b7ec8acfb8ffd72
Andrey Burgos <95008084+DreyOWO@users.noreply.github.com>
2024-07-23 01:20:43 -0600

    Adding validation for the client/proveedor rol

--- controllers/controllerLogin.php
+++ controllers/controllerLogin.php
@@ -11,19 +11,25 @@ if (isset($_POST["btningresar"])) {
         $nombre_usuario = $_POST["nombre_usuario"];
         $contraseña = $_POST["contraseña"];
 
-        $stmt = $conexion->prepare("SELECT * FROM Usuarios WHERE nombre_usuario = ? AND contraseña = ?");
-        $stmt->bind_param("ss", $nombre_usuario, $contraseña);
+        // Consulta para obtener la contraseña y el rol del usuario
+        $stmt = $conexion->prepare("SELECT contraseña, rol FROM Usuarios WHERE nombre_usuario = ?");
+        $stmt->bind_param("s", $nombre_usuario);
         $stmt->execute();
-        $result = $stmt->get_result();
+        $stmt->bind_result($db_contraseña, $rol);
+        $stmt->fetch();
 
-        if ($result->fetch_object()) {
-
-            header("Location: indexDashboard.html");
+        // Verificar si la contraseña ingresada coincide con la almacenada
+        if ($contraseña === $db_contraseña) {
+            // Redirigir según el rol
+            if ($rol == 'proveedor') {
+                header("Location: indexDashboard.html");
+            } else if ($rol == 'cliente') {
+                header("Location: index.html");
+            }
             exit();
         } else {
             echo "<div class='alert alert-danger' role='alert'>Usuario o contraseña incorrectos</div>";
         }
-
 
         $stmt->close();
     }
