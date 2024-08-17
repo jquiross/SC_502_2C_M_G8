6c76e226c35549f7fea3a0056447ba0d1959a937
Emmanuel8112 <earaya00259@ufide.ac.cr>
2024-08-17 17:13:07 -0600

    Index con productos de la bd

---
+++ controllers/controllerProductos.php
@@ -0,0 +1,23 @@
+<?php
+//conexion a bd
+include("./config/conexion.php");
+
+//se crea una clase para que el manejo sea mas facil
+class controllerProductos {
+    //se hace la funcion de la consulta
+    public function getProducts() {
+        global $conexion;
+        $stmt = $conexion->prepare("SELECT nombre_producto, descripcion, precio, descuento, img_ruta FROM Productos");
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        $products = [];
+        while ($row = $result->fetch_assoc()) {
+            $products[] = $row;
+        }
+
+        $stmt->close();
+        return $products;
+    }
+}
+?>
